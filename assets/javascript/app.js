07c99dcb78a3ea5ec3f27080f2f1404541a0dcfe
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-09-22 21:46:47 -0600

    more functions need more work, created searchbar button maker

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,70 @@
 // global Var's here. 
 
 
+var gifDiv = $('<div id="gifs">');
 
-// create click event to register our search box to search variable
-$('.searchbtn').on("click", function () {
+// array full of the pre-made buttons 
+var topics = ["planets", "moon", "astronauts", "Sun", "Earth", "Space Station", "galaxy", "Nebula", "Space Ship", "Black Hole"]
+// dynamically create a div(s) for the gifs & buttons to go.
+function makeBtns() {
+    $('.buttons').empty();
+    for (var i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+        // var that holds buttons
+        var catBtn = $('<button>')
+        // add class, attribute, and text to buttons
+        catBtn.addClass('topic');
+        catBtn.text(topics[i]);
+        catBtn.attr('data-name', topics[i]);
+        // append to div
+        $('.buttons').append(catBtn);
+    }
+};
+makeBtns();
 
-    // create Var to hold search term 
-    var search = $('#searchBar').val().trim()
-    
-    // use AJAX to get API info from GIPHY
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&limit=8&rating=g&api_key=zeDT6SboXDp0gp0a6v5WKywQmmGLZuyS"
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        var results = (response.data)
-        
+// function to display the gifs
+function displayGifs() {
+
+    var topicID = $('.topic').click(function () {
+        var item = $(this).attr('data-name');
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + item + "&limit=8&rating=g&api_key=zeDT6SboXDp0gp0a6v5WKywQmmGLZuyS"
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            var results = (response.data)
+            for (i = 0; i < results.length; i++) {
+                var gifDiv = $('<div>');
+                var rating = results[i].rating;
+                var p = $('<p>').text("Rating: " + rating);
+                var image = $('<img>');
+                image.attr('src', results[i].images.fixed_height.url);
+
+                gifDiv.prepend(p);
+                gifDiv.prepend(image);
+                $('gifArea').prepend(gifDiv);
+            }
+        })
     });
-})
-// dynamically create a div(s) for the gifs to go.
-// append user searches to the dom where the buttons are.  
-// create click function to change the state of the gifs, still and animate. 
+};
+displayGifs();
+
+
+// make Search bar input make a new button
+$('.searchbtn').on("click", function () {
+    event.preventDefault();
+    // create Var to hold search term 
+    var search = $('#searchBar').val().trim();
+    console.log(search);
+    topics.push(search);
+    console.log(topics);
+    makeBtns();
+    $('#searchBar').val("");
+});
+
+// TO DO: 
+// ========================================================================================================
+// create click event to register our search box to search variable, push new button up to topic array and rerun make button function
